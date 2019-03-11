c157bf4c5469d794876ec29721dc042dfee132c1
Ailene-Hoang <aileneh2020@gmail.com>
2019-03-10 22:01:40 -0700

    Initial commit

---
+++ src/components/ToDoItem.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function ToDoItem() {
+    return (
+        <div className='listItem'>
+            <input type='checkbox' />
+            <span>  Text</span><br />
+        </div>
+    )
+}
+
+export default ToDoItem
