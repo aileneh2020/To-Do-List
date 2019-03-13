4e5dab41ed803d3c26c446ad7641c29db5386f21
Ailene-Hoang <aileneh2020@gmail.com>
2019-03-12 20:23:20 -0700

    Phase 4 Change App from function to class based component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
-import React from 'react'
+import React, {Component} from 'react'
 import ToDoItem from './components/ToDoItem'
-import todoData from './todoData'
+import todoData from './data/todoData'
 import './styles.css'
 
-function App() {
-    const todoComponents = todoData.map(item => 
-        <ToDoItem 
-            key={item.id}
-            todo={item}
-        />
-    )
-    return (
-        <div className='container'>
-            <h1>To Do List</h1>
-            {todoComponents}
-        </div>
-    )
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            todos: todoData
+        }
+    }
+    
+    render() {
+        const todoComponents = this.state.todos.map(item => 
+            <ToDoItem 
+                key={item.id}
+                todo={item}
+            />
+        )
+        return (
+            <div className='container'>
+                <h1>To Do List</h1>
+                {todoComponents}
+            </div>
+        )
+    }
 }
 
 export default App
