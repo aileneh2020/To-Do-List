bf8a76551e395b782954042cab83427759a45591
Ailene-Hoang <aileneh2020@gmail.com>
2019-03-19 21:22:45 -0700

    Phase 5 Add onChange property to check box

--- src/components/ToDoItem.js
+++ src/components/ToDoItem.js
@@ -1,12 +1,27 @@
-import React from 'react'
+import React, {Component} from 'react'
 
-function ToDoItem(props) {
-    return (
-        <div className='listItem'>
-            <input type='checkbox' checked={props.todo.completed}/>
-            <span> {props.todo.text}</span><br />
-        </div>
-    )
+class ToDoItem extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    toggleCheckMark = () => {
+        console.log('changed')
+    }
+
+    render() {
+        return (
+            <div className='listItem'>
+                <input
+                    type='checkbox' 
+                    checked={this.props.todo.completed}
+                    onChange={this.toggleCheckMark}
+                />
+                <span> {this.props.todo.text}</span><br />
+            </div>
+        )
+
+    }
 }
 
 export default ToDoItem
